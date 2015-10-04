38ff204763d1cc62baffed8df412a182252a01a1
Jonatas C Damasceno <jonatas.cd@gmail.com>
2015-10-04 14:18:37 -0300

    habilita admin das apps base e contas

--- banco/base/admin.py
+++ banco/base/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from models import Perfil, Banco
+
+admin.site.register(Perfil)
+admin.site.register(Banco)
--- banco/contas/admin.py
+++ banco/contas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from models import Agencia, Conta, Transacao
+
+admin.site.register(Agencia)
+admin.site.register(Conta)
+admin.site.register(Transacao)
