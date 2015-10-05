b7862bbcf7cc927689ea996b31f00d05c641a5f0
Jonatas C Damasceno <jonatas.cd@gmail.com>
2015-10-05 20:52:14 -0300

    corrige import circular

--- banco/base/signals.py
+++ banco/base/signals.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
-from contas.models import Conta
 
 def perfil_post_save(sender, **kwargs):
+    from contas.models import Conta
     perfil = kwargs['instance']
     Conta.cria_conta(perfil)
