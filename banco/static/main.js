81ba4c6b9ed2e9a3dc1a23dbc6ae3c4db03b7d8c
Jonatas C Damasceno <jonatas.cd@gmail.com>
2015-10-05 21:50:48 -0300

    ajax de dashboard

--- banco/static/main.js
+++ banco/static/main.js
@@ -1,6 +1,24 @@
 console.log('aqui!');
 
 $(document).ready(function() {
+  $("#dashboard").on("click", function (event) {
+    event.preventDefault();
+    $('.active').removeClass('active')
+    $.ajax({
+      url: '/dashboard',
+      dataType: 'html',
+      method: 'get',
+      success: function(dados){
+        $('#container-fluid').html(dados);
+        $("#dashboard").parents().addClass('active');
+        console.log('dash');
+      },
+      error: function(){
+        console.log('erro no dashboard');
+      }
+    });
+  });
+
   $("#extrato").on("click", function (event) {
     event.preventDefault();
     $('.active').removeClass('active')
